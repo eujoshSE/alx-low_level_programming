5cafdd4f2aef264d77eefb9de288cc95942320ac
eujoshSE <eujosh.w@gmail.com>
2023-10-27 17:13:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point of the program
+* @argc: The number of argument
+* @argv: An array of pointers
+* Return: Always 0.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
